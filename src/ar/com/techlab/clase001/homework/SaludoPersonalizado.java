72fbaf711030d492dfcb0e00d99fc9a2bf65ec90
Juan_Lima <juan_jpl@hotmail.com>
2025-11-20 23:50:31 -0300

    update class001 project

---
+++ src/ar/com/techlab/clase001/homework/SaludoPersonalizado.java
@@ -0,0 +1,7 @@
+package ar.com.techlab.clase001.homework;
+
+public class SaludoPersonalizado {
+    public static void main(String[] args) {
+        System.out.println("¡Bienvenides al mundo de Java!");
+    }
+}
---
+++ src/ar/com/techlab/clase002/homework/MainHomework002.java
@@ -0,0 +1,7 @@
+package ar.com.techlab.clase002.homework;
+
+public class MainHomework002 {
+    public static void main(String[] args){
+        System.out.println("Homework de la clase 002 - Estructura de datos!");
+    }
+}
