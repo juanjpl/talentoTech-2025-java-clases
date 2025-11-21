3c045bdd5f181192a812405c97438859e9c6b281
Juan_Lima <juan_jpl@hotmail.com>
2025-11-21 19:01:42 -0300

    update class002 project

--- src/ar/com/techlab/clase001/practica/MainPractica001.java
+++ src/ar/com/techlab/clase001/practica/MainPractica001.java
@@ -4,7 +4,7 @@ public class MainPractica001 {
     public static void main(String[] args){
         System.out.println("Hola mundo desde Intellij");
         System.out.println("Ahora vamos a probar estructura de datos!");
-        System.out.println("Ahora usemos emojies \uD83C\uDF84  \uD83E\uDD73 ☺\uFE0F\u200B\uD83C\uDF4E\u200B\uD83C\uDF4F\u200B");
+        System.out.println("Ahora usemos emojies \uD83C\uDF84  \uD83E\uDD73\u200B\uD83C\uDF4E\u200B\uD83C\uDF4F\u200B");
         String mensaje ="Estamos usando una variable tipo String";
         System.out.println(mensaje);
     }
