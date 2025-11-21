866879cbd2d9a62548718dbaee2f5378824988e4
Juan_Lima <juan_jpl@hotmail.com>
2025-11-20 22:12:39 -0300

    update class001 project

--- src/ar/com/techlab/clase001/MainClase001.java
+++ src/ar/com/techlab/clase001/MainClase001.java
@@ -3,5 +3,6 @@ package ar.com.techlab.clase001;
 public class MainClase001 {
     public static void main(String[] args){
         System.out.println("Hola mundo desde Intellij");
+        System.out.println("Ahora vamos a probar estructura de datos!");
     }
 }
