33fa2046f97ba1eabf30ec5be84a804f295beb6d
Juan_Lima <juan_jpl@hotmail.com>
2025-11-20 23:50:31 -0300

    update class001 project

--- src/ar/com/techlab/clase001/practica/MainPractica001.java
+++ src/ar/com/techlab/clase001/practica/MainPractica001.java
@@ -1,6 +1,6 @@
-package ar.com.techlab.clase001;
+package ar.com.techlab.clase001.practica;
 
-public class MainClase001 {
+public class MainPractica001 {
     public static void main(String[] args){
         System.out.println("Hola mundo desde Intellij");
         System.out.println("Ahora vamos a probar estructura de datos!");
---
+++ src/ar/com/techlab/clase002/practica/MainPractica002.java
@@ -0,0 +1,7 @@
+package ar.com.techlab.clase002.practica;
+
+public class MainPractica002 {
+    public static void main(String[] args){
+        System.out.println("Esta es la segunda clase! Estructura de datos...");
+    }
+}
