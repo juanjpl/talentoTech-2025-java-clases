57e09b41439afc98ac4dda3e7e879ee325fe889d
juanjpl <juan_jpl@hotmail.com>
2025-11-27 16:06:08 -0300

    upload pre-entrega

--- src/ar/com/techlab/clase001/after/SaludoPersonalizado.java
+++
@@ -1,7 +0,0 @@
-package ar.com.techlab.clase001.after;
-
-public class SaludoPersonalizado {
-    public static void main(String[] args) {
-        System.out.println("¡Bienvenides al mundo de Java!");
-    }
-}
--- src/ar/com/techlab/clase005/Main.java
+++
@@ -1,4 +0,0 @@
-package ar.com.techlab.clase005;
-
-public class Main {
-}
